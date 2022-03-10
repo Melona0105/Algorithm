d92ab7404a0b83b8dcd149907c6931d8c9495c48
Deokwon <deokwon.park@sharexpere.com>
2022-03-10 15:30:58 +0900

    fix : tsc using ES6

--- Study/220305/GetIP.js
+++ Study/220305/GetIP.js
@@ -1,39 +1,41 @@
 function getIPAddress(ipString) {
-    var answer = [];
-    for (var i = 1; i < 4; i++) {
-        var firstPart = ipString.slice(0, i);
-        if (!isValidIP(firstPart)) {
-            continue;
-        }
-        for (var j = i; j < i + 4; j++) {
-            var secondPart = ipString.slice(i, j);
-            if (!isValidIP(secondPart)) {
-                continue;
-            }
-            for (var k = j; k < j + 4; k++) {
-                var thirdPart = ipString.slice(j, k);
-                if (!isValidIP(thirdPart)) {
-                    continue;
-                }
-                for (var m = k; m < k + 4; m++) {
-                    var last = ipString.slice(k, m);
-                    if (!isValidIP(last)) {
-                        continue;
-                    }
-                    if (firstPart && secondPart && thirdPart && last) {
-                        answer.push("".concat(firstPart, ".").concat(secondPart, ".").concat(thirdPart, ".").concat(last));
-                    }
-                }
-            }
-        }
+  const answer = [];
+  for (let i = 1; i < 4; i++) {
+    const firstPart = ipString.slice(0, i);
+    if (!isValidIP(firstPart)) {
+      continue;
     }
-    return answer;
+    for (let j = i; j < i + 4; j++) {
+      const secondPart = ipString.slice(i, j);
+      if (!isValidIP(secondPart)) {
+        continue;
+      }
+      for (let k = j; k < j + 4; k++) {
+        const thirdPart = ipString.slice(j, k);
+        if (!isValidIP(thirdPart)) {
+          continue;
+        }
+        for (let m = k; m < k + 4; m++) {
+          const last = ipString.slice(k, m);
+          if (!isValidIP(last)) {
+            continue;
+          }
+          if (firstPart && secondPart && thirdPart && last) {
+            answer.push(`${firstPart}.${secondPart}.${thirdPart}.${last}`);
+          }
+        }
+      }
+    }
+  }
+  return answer;
 }
 function isValidIP(ipNum) {
-    return !(+ipNum > 255 ||
-        ipNum.length > 4 ||
-        (ipNum[0] === "0" && ipNum.length >= 2));
+  return !(
+    +ipNum > 255 ||
+    ipNum.length > 4 ||
+    (ipNum[0] === "0" && ipNum.length >= 2)
+  );
 }
-var a = "19216801";
-var result = getIPAddress(a);
+const a = "19216801";
+const result = getIPAddress(a);
 console.log(result);
--- Study/220305/String.js
+++ Study/220305/String.js
@@ -1,7 +1,7 @@
 function convertFunc(input) {
-    var isNeg = false;
+    let isNeg = false;
     if (typeof input === "string") {
-        var result = 0;
+        let result = 0;
         if (input[0] === "-") {
             isNeg = true;
             result = searchString(1, input, result, isNeg);
@@ -12,14 +12,14 @@ function convertFunc(input) {
         return result;
     }
     else if (typeof input === "number") {
-        var result = "";
+        let result = "";
         if (input < 0) {
             result = "-";
             input = Math.abs(input);
         }
-        var numberLength = getNumLength(input);
-        for (var i = numberLength; i >= 1; i--) {
-            var now = Math.round(input / Math.pow(10, i - 1));
+        const numberLength = getNumLength(input);
+        for (let i = numberLength; i >= 1; i--) {
+            const now = Math.round(input / Math.pow(10, i - 1));
             result = result + numObj[now];
             input = input - now * Math.pow(10, i - 1);
         }
@@ -27,10 +27,10 @@ function convertFunc(input) {
     }
 }
 function searchString(startIndex, input, result, isNeg) {
-    for (var i = startIndex; i < input.length; i++) {
-        var now = input[i];
+    for (let i = startIndex; i < input.length; i++) {
+        const now = input[i];
         // 자릿수
-        var numlocation = Math.pow(10, input.length - i - 1);
+        const numlocation = Math.pow(10, input.length - i - 1);
         if (isNeg) {
             result = result + strObj[now] * numlocation * -1;
         }
@@ -41,15 +41,15 @@ function searchString(startIndex, input, result, isNeg) {
     return result;
 }
 function getNumLength(input) {
-    var current = 1;
-    var length = 0;
+    let current = 1;
+    let length = 0;
     while (current <= input) {
         current = current * 10;
         length++;
     }
     return length;
 }
-var strObj = {
+const strObj = {
     "1": 1,
     "2": 2,
     "3": 3,
@@ -59,9 +59,9 @@ var strObj = {
     "7": 7,
     "8": 8,
     "9": 9,
-    "0": 0
+    "0": 0,
 };
-var numObj = {
+const numObj = {
     "1": "1",
     "2": "2",
     "3": "3",
@@ -71,9 +71,9 @@ var numObj = {
     "7": "7",
     "8": "8",
     "9": "9",
-    "0": "0"
+    "0": "0",
 };
-var strToNum = convertFunc("-123");
+const strToNum = convertFunc("-123");
 // console.log(strToNum);
-var numToStr = convertFunc(-123);
+const numToStr = convertFunc(-123);
 console.log(numToStr);
